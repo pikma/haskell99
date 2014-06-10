0045f5b587681f335e3f6a4e6798e378cb0ce1f1
Pierre Kreitmann <pierre.kreitmann@gmail.com>
2014-06-09 22:37:36 -0700

    Done with lists

--- lists.hs
+++ lists.hs
@@ -4,3 +4,41 @@
 myLast [] = error "empty list"
 myLast [x] = x
 myLast (_:xs) = myLast xs
+
+myButLast [] = error "empty list"
+myButLast [x] = error "list of one element"
+myButLast [x, _] = x
+myButLast (_:xs) = myButLast xs
+
+elementAt [] _ = error "empty list"
+elementAt l 1 = head l
+elementAt l n = elementAt (tail l) (n - 1)
+
+myLength l = myLength' l 0 where
+  myLength' [] n = n
+  myLength' (_:xs) n = myLength' xs (n+1)
+
+myReverse = foldl (flip (:)) []
+
+isPalyndrome l = (reverse l) == l
+
+data NestedList a = Elem a | List [NestedList a]
+flatten (Elem x) = [x]
+flatten (List []) = []
+flatten (List (x:xs)) = (flatten x) ++ (flatten $ List xs)
+-- To remember: concatMap :: (a -> [b]) -> [a] -> [b]
+
+compress (x1:l@(x2:xs))
+  | x1 == x2  = compress l
+  | otherwise = x1:(compress l)
+compress l = l
+
+pack [] = []
+pack (x:xs) = reverse $ pack' xs [x] []
+  where pack' []     xl xll = xl:xll
+        pack' (x:xs) xl xll
+          | x == head xl = pack' xs (x:xl) xll
+          | otherwise    = pack' xs [x] (xl:xll)
+-- To remember: span :: (a -> Bool) -> [a] -> ([a], [a])
+
+encode l = map (\x -> (length x, head x)) (pack l)
