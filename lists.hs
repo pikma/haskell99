f76e0030f68419a34d807fbc5e96097d38f2eb34
Pierre Kreitmann <kreitmann@google.com>
2014-06-11 23:00:06 -0700

    Problem 14 is solved.

--- lists.hs
+++ lists.hs
@@ -42,3 +42,36 @@ pack (x:xs) = reverse $ pack' xs [x] []
 -- To remember: span :: (a -> Bool) -> [a] -> ([a], [a])
 
 encode l = map (\x -> (length x, head x)) (pack l)
+
+-- Solutions to the problems 1-10, at
+-- http://www.haskell.org/haskellwiki/99_questions/11_to_20
+
+data Encoding a = Multiple (Int, a) | Single a
+  deriving (Show)
+encodeModified l = map aux $ encode l
+  where aux (n, x) = if n == 1 then Single x else Multiple (n, x)
+-- Other solution, that doesn't reuse anything from above:
+encodeModified' l = reverse $ aux l []
+  where aux []       ll = ll
+        aux l@(x:xs) ll =
+          let (begin, end) = span (== x) l
+              count = length begin
+              compressed = if count == 1 then Single x
+                                         else Multiple (count, x) in
+                aux end (compressed:ll)
+
+decodeModified = reverse . (foldl f [])
+  where f res (Single x) = x:res
+        f res (Multiple (n, x)) = (replicate n x) ++ res
+-- Note: should have used concatMap.
+
+encodeDirect l = reverse $ aux l [] where
+  aux []      ll = ll
+  aux l@(x:_) ll =
+    let (n, rest) = takeWhileAndCount l in
+      aux rest (encodeElement (n, x):ll) where
+        encodeElement (1, x) = Single x
+        encodeElement (n, x) = Multiple (n, x)
+        takeWhileAndCount l = aux' 0 l where
+          aux' n [] = (n, [])
+          aux' n l@(x':xs) = if x' == x then aux' (1+n) xs else (n, l)
